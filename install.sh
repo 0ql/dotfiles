2d8c59f948e579c9501f5997706f9ef9a4b01e3f
Kaffee <tobi.von.massow@gmail.com>
2022-05-22 17:12:03 +0200

    install script

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "This will override ~/.config/[ awesome, nvim, starship, zsh, kitty]."
+echo -n "Are you sure you want to proceed? [y/N] "
+read input
+
+if [[ $input != "y" ]]; then
+  exit
+fi
+
+dotconfig="/home/$USER/.config"
+
+if [[ ! -d $dotconfig ]]; then
+  mkdir $dotconfig
+fi
+
+if [[ ! -d "./.config" || ! -d "../dotfiles" ]]; then
+  echo "Please run the script in the repo directory."
+  exit
+fi
+
+cp ./.config/zsh $dotconfig
+cp ./.config/kitty $dotconfig
+cp ./.config/awesome $dotconfig
+cp ./.config/starship $dotconfig
+
+cd $dotconfig
+git clone https://github.com/0ql/LunarVim.git nvim
+
+echo -n "Install required software? ArchLinux only (requires pacman) [y/N] "
+read input
+
+if [[ $input != "y" ]]; then
+  exit
+fi
+
+sudo pacman --noconfirm -S kitty zsh awesome starship neovim
