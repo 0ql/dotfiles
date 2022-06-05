eb791d30c0a07e9d46316b6e5351f9e96f4c46eb
Kaffee <tobi.von.massow@gmail.com>
2022-06-05 19:15:33 +0200

    disable scrolling with mouse through tags

--- .config/awesome/modules/keybindings.lua
+++ .config/awesome/modules/keybindings.lua
@@ -4,9 +4,7 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 
 -- Mouse bindings
 root.buttons(gears.table.join(
-  awful.button({}, 3, function() mymainmenu:toggle() end),
-  awful.button({}, 4, awful.tag.viewnext),
-  awful.button({}, 5, awful.tag.viewprev)
+  awful.button({}, 3, function() mymainmenu:toggle() end)
 ))
 
 -- keybindings
